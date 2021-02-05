0cc74162fbfbbc52cb6514565623b9b25a198b14
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2021-02-05 18:08:23 +0900

    indexページ実装

--- app/javascript/modals.js
+++ app/javascript/modals.js
@@ -13,3 +13,33 @@ window.addEventListener('turbolinks:load', ()=>{
   document.getElementById("mask").classList.remove("active"); 
  });
 });
+
+window.addEventListener('turbolinks:load', ()=>{
+  const modalObject = document.getElementById("modal-class");
+ modalObject.addEventListener('click', ()=>{
+  document.getElementById("modal-content").classList.add("active");
+  document.getElementById("hobit-mask").classList.add("active"); 
+ });
+});
+
+window.addEventListener('turbolinks:load', ()=>{
+  const modalObject = document.getElementById("hobit-close");
+ modalObject.addEventListener('click', ()=>{
+  document.getElementById("modal-content").classList.remove("active");
+  document.getElementById("hobit-mask").classList.remove("active"); 
+ });
+});
+
+window.addEventListener('turbolinks:load', ()=>{
+  const modalObject = document.getElementById("modal-userIndex");
+ modalObject.addEventListener('click', ()=>{
+  document.getElementById("user-contents").classList.add("active");
+ });
+});
+
+window.addEventListener('turbolinks:load', ()=>{
+  const modalObject = document.getElementById("user-close");
+ modalObject.addEventListener('click', ()=>{
+  document.getElementById("user-contents").classList.remove("active");
+ });
+});
--- app/javascript/top.js
+++ app/javascript/top.js
@@ -9,3 +9,27 @@ for (let i = 0; i< targetElement.length; i++){
     }
 }
 });
+
+window.document.addEventListener('scroll',()=>{
+  const targetElement = document.querySelectorAll(".contents-text");
+  for (let i = 0; i< targetElement.length; i++){
+    const getElementDistance =targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6;
+      if(window.innerHeight > getElementDistance){
+        targetElement[i].classList.add("show");
+      }else if(window.innerHeight < getElementDistance){
+        targetElement[i].classList.remove("show");
+      }
+  }
+  });
+
+  window.document.addEventListener('scroll',()=>{
+    const targetElement = document.querySelectorAll(".second-text");
+    for (let i = 0; i< targetElement.length; i++){
+      const getElementDistance =targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6;
+        if(window.innerHeight > getElementDistance){
+          targetElement[i].classList.add("show");
+        }else if(window.innerHeight < getElementDistance){
+          targetElement[i].classList.remove("show");
+        }
+    }
+    });
