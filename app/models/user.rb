96a5d8fa9d2c85cff15a4b18d98cfe54adec4309
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2021-03-03 21:28:11 +0900

    フォロー＆フォロワー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,26 @@ has_many :likes
 has_many :comments 
 has_many :aggregates  
 has_many :favorites, dependent: :destroy
+has_many :relationships
+has_many :followings, through: :relationships, source: :follow
+has_many :reverse_of_reationships, class_name: 'Relationship', foreign_key: 'follow_id'
+has_many :followers, through: :reverse_of_reationships, source: :user
 mount_uploader :image, ImageUploader
+
+def follow(other_user)
+  unless self == other_user
+    self.relationships.find_or_create_by(follow_id: other_user.id)
+  end
+end
+
+def unfollow(other_user)
+  relationship = self.relationships.find_by(follow_id: other_user.id)
+  relationship.destroy if relationship
+end
+
+def following?(other_user)
+  self.followings.include?(other_user)
+end
 
   def liked_by?(objective_id)
     likes.where(objective_id: objective_id).exists?
--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,6 @@ Rails.application.routes.draw do
     end
   end
 
-  
-  
-
+  resources :relationships, only: [:create, :destroy]
   
 end
