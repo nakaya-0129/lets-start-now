9c23da44e1657a0619106e0ad9d51ca5602b5216
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2021-03-06 18:29:51 +0900

    不要記述の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,6 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
    @objectives = @user.objectives
-
-   favorites = Favorite.where(user_id: current_user.id).pluck(:objective_id)
-   @favorite_list = Objective.find(favorites)
   end
 
 end
